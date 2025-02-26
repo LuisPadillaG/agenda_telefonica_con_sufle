08ecdb7fbea5db321c9f2d459aa0989a9494a0c3
BRUNO GIBRAN REYES ALVAREZ <al205757@alumnos.uacj.mx>
2025-02-26 10:14:35 -0700

    [AGREGAR] un componente para la vista de un contacto prevista

---
+++ Agenda Shufle/Agenda Shufle/componentes/contacto_prevista.swift
@@ -0,0 +1,18 @@
+//
+//  contacto_prevista.swift
+//  Agenda Shufle
+//
+//  Created by alumno on 2/26/25.
+//
+
+import SwiftUI
+
+struct contacto_prevista: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    contacto_prevista()
+}
